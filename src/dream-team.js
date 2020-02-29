7e42154bcb928686b3b8cb4413155960751cb046
Gleb Zinoviev <gz0609@gmail.com>
2020-02-29 23:58:05 +0300

    feat: add solutions

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,12 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  var newArr = [];
+  if ( members == null){
+    return false;
+  }
+  for(var i=0; i < members.length; i++){
+  if (typeof members[i] == "string"){
+    members[i] = members[i].replace(/\s+/g,'');
+    newArr.push(members[i][0].toUpperCase());
+  }
+ }  return newArr.sort().join('');
+}
